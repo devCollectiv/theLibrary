51e308e821bcf3308aeab0aa056937fe1d67669b
kiel-h-byrne <kiel.byrne@gmail.com>
2024-08-14 17:41:58 -0400

    add addon

--- React/comp_library/.storybook/main.ts
+++ React/comp_library/.storybook/main.ts
@@ -1,13 +1,26 @@
+import path from "node:path";
+
 /** @type { import('@storybook/react-webpack5').StorybookConfig } */
 const config = {
   stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
   addons: [
     "@storybook/preset-create-react-app",
     "@storybook/addon-links",
+    "@storybook/addon-storysource",
     "@storybook/addon-essentials",
     "@chromatic-com/storybook",
     "@storybook/addon-interactions",
-    '@chakra-ui/storybook-addon'
+    '@chakra-ui/storybook-addon',
+    {
+      name: "@storybook/addon-storysource",
+      options: {
+        rule: {
+          include: [path.resolve(__dirname, '../src/components/')], // You can specify directories
+        },
+        loaderOptions: {
+          prettierConfig: { printWidth: 100, singleQuote: false },
+        },
+    }
   ],
   framework: {
     name: "@storybook/react-webpack5",
